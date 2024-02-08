63184a21196a53111502fdd0fe373f48ba7954b6
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-08 13:52:12 +0100

    base structure, base idea

---
+++ main.py
@@ -0,0 +1,14 @@
+# Quizz
+
+questions = ("",
+            "",
+            "",
+            "",
+            "")
+
+options = ((),(),(),(),())
+
+answers = ()
+guesses = []
+score = 0
+questionNumber = 0
