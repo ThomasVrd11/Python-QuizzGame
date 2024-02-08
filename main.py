f8ff0af79d1b07de711c020a4e602044ebebf046
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-08 13:58:11 +0100

    Questions updated

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Quizz
 
-questions = ("",
-            "",
-            "",
-            "",
-            "")
+questions = ("In which video game do players explore the fictional post-apocalyptic United States in the character of Joel and Ellie?", 
+            "What is the name of the main protagonist in the video game series 'The Legend of Zelda'?",
+            "Which game is known for the iconic phrase 'War... War never changes'?",
+            "In Minecraft, what do you need to mine in order to obtain diamonds?",
+            "'Overwatch' is a team-based multiplayer first-person shooter developed and published by which company?")
 
 options = ((),(),(),(),())
 
